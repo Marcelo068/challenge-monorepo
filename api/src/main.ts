199be2f8ee1ba5eb04a54953f1d97c28768a573c
Marcelo da Silva <marcelor068@gmail.com>
2022-04-19 12:20:30 -0300

    fix(): try to fix error of socket.io

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 import { AllExceptionsFilter } from './app/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
